7d4112fc453ef5a8519e52fb057cb497eccf7dff
LuisaFC <luisabsb@gmail.com>
2022-05-01 23:59:37 -0300

    Page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+   <>
+     <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <br/><span>React</span> world.</h1>
+          <p>
+            Get access to all the contents <br />
+            <span>for only $9.90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+   </>
   )
 }
